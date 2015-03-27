eed024f22ef24eb931e550dfa336a7e5600abd81
Fred Chapman <areinei123@gmail.com>
2015-03-27 13:13:07 -0400

    Added requirements
    to restaurants show view to add reviews

--- good_eats/config/routes.rb
+++ good_eats/config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :restaurants, only:[:index, :show, :new, :create] do
     resources :reviews, only:[:index, :new, :create]
   end
+
+  get "/restaurants/#{@restaurant_id}" => 'reviews#index'
 end
